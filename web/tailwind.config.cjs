5f6ca14eea7ba67e2733781dbeddd64a19e1ef78
Raphael Soares <raphael.moreira@interconstrutora.com.br>
2022-09-14 14:34:59 -0300

    Final aula 2

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,10 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Sans: ['Inter', 'sans-serif']
+      },
       backgroundImage:{
         galaxy: "url('/bg.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
-        'game-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
